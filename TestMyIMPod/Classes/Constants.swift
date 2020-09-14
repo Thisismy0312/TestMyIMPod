ded2ac10accdafec00e16bf41c847b685ff956c9
miles <qq867327466@163.com>
2020-09-14 13:38:09 +0800

    fix

---
+++ TestMyIMPod/Classes/Constants.swift
@@ -0,0 +1,5 @@
+let screenSize = UIScreen.main.bounds
+let screenWidth = screenSize.width
+let screenHeight = screenSize.height
+let unmistakableChars
+ = "23456789ABCDEFGHJKLMNPQRSTWXYZabcdefghijkmnopqrstuvwxyz"
